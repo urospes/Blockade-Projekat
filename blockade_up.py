9338684f67010eb55e68d0ad11283d343befb5ad
unknown <pesic.uros@hotmail.com>
2021-12-12 20:46:04 +0100

    igrica

--- blockade_up.py
+++ blockade_up.py
@@ -7,6 +7,9 @@ pygame.init()
 
 game = bl.Game()
 game.getStartState()
+const.SQUARE = 40 if game.board.m < 13 else 30 if game.board.m < 18 else 24
+const.WALL_W = 10 if game.board.m < 13 else 7
+const.WALL_H = 2 * const.SQUARE + const.WALL_W
 
 screen = pygame.display.set_mode((const.WIN_WIDTH, const.WIN_HEIGHT))
 screen.fill(const.WIN_COLOR)
@@ -33,7 +36,7 @@ def DrawHeader(p1_walls: tuple[int, int], p2_walls: tuple[int, int], redFirst: b
     text_surface_next_move = text_font.render(next_player_text, True, "Gray")
     text_rect_next_move = text_surface_next_move.get_rect(center = (const.WIN_WIDTH // 2, const.TEXT_OFFSET + const.FONT_SIZE // 2))
 
-
+    screen.fill("BLACK")
     screen.blit(text_surface_p1, text_rect_p1)
     screen.blit(text_surface_p2, text_rect_p2)
     screen.blit(text_surface_next_move, text_rect_next_move)
@@ -67,7 +70,7 @@ def DrawHeader(p1_walls: tuple[int, int], p2_walls: tuple[int, int], redFirst: b
 
 
 #funckija za crtanje oznaka kolona i vrsta
-def DrawLabels(game_rect: pygame.Rect, size: tuple[int, int]):
+def DrawLabels(game_rect: pygame.Rect, size: tuple[int, int], offset: int):
 
     #sve vezano za tablu crtamo u okviru game_rect pravougaonika
     labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"];
@@ -78,13 +81,13 @@ def DrawLabels(game_rect: pygame.Rect, size: tuple[int, int]):
     #oznake vrsta
     for i in range(0, size[0]):
         label_surface = label_font.render(str(labels[i]), False, "WHITE")
-        label_rect = pygame.rect.Rect(game_rect.left + const.WALL_W, game_rect.top + (i + 1) * (const.SQUARE + const.WALL_W), const.SQUARE, const.SQUARE)
+        label_rect = pygame.rect.Rect(game_rect.left + const.WALL_W + offset, game_rect.top + (i + 1) * (const.SQUARE + const.WALL_W), const.SQUARE, const.SQUARE)
         screen.blit(label_surface, label_rect)
 
     #oznake kolona
     for i in range(0, size[1]):
         label_surface = label_font.render(str(labels[i]), False, "WHITE")
-        label_rect = pygame.rect.Rect(game_rect.left + (i + 1) * (const.WALL_W + const.SQUARE), game_rect.top + const.WALL_W, const.SQUARE, const.SQUARE)
+        label_rect = pygame.rect.Rect(game_rect.left + (i + 1) * (const.WALL_W + const.SQUARE) + offset, game_rect.top + const.WALL_W, const.SQUARE, const.SQUARE)
         screen.blit(label_surface, label_rect)
 
 
@@ -95,7 +98,7 @@ def DrawWalls(board_rect: pygame.Rect, walls: bl.Wall):
     for wall in walls:
         wall_rect = None
         color = None
-        if wall.type == "blue":
+        if wall.type == "p":
             left = board_rect.left + wall.position[1] * (const.SQUARE + const.WALL_W)
             top = board_rect.top + (wall.position[0] + 1) * (const.SQUARE + const.WALL_W) - const.WALL_W
             wall_rect = pygame.rect.Rect(left, top, const.WALL_H, const.WALL_W)
@@ -110,44 +113,51 @@ def DrawWalls(board_rect: pygame.Rect, walls: bl.Wall):
             pygame.draw.rect(screen, color, wall_rect)
 
 
+
 #funckija za iscrtavanje trenutnog stanja na tabli
-def DrawGameBoard(size: tuple[int, int], player_1: tuple[tuple[int, int]], player_2: tuple[tuple[int, int]], walls: list[tuple[int, int]]):
+def DrawGameBoard(size: tuple[int, int], player_1: tuple[tuple[int, int]], player_2: tuple[tuple[int, int]], walls: list[tuple[int, int]], start_positions):
 
     game_rect = pygame.rect.Rect(const.GAME_BOARD_X, const.GAME_BOARD_Y, const.GAME_BOARD_W, const.GAME_BOARD_H)
     pygame.draw.rect(screen, const.GAME_RECT_COLOR, game_rect, 3)
 
-    DrawLabels(game_rect, size)
-
     board_w = size[1] * const.SQUARE + (size[1] - 1) * const.WALL_W
     board_h = size[0] * const.SQUARE + (size[0] - 1) * const.WALL_W
-    board_rect = pygame.rect.Rect(game_rect.left + const.WALL_W + const.SQUARE, game_rect.top + const.WALL_W + const.SQUARE, board_w, board_h)
+    offset = (game_rect.width - board_w) // 2
+    DrawLabels(game_rect, size, offset)
+
+    board_rect = pygame.rect.Rect(game_rect.left + const.WALL_W + const.SQUARE + (game_rect.width - board_w) // 2, game_rect.top + const.WALL_W + const.SQUARE, board_w, board_h)
 
     #crtamo polja u odnosu na pravougaonik board_rect
     for i in range(0, size[0]):
         for j in range(0, size[1]):
             square = pygame.rect.Rect(board_rect.left + j * (const.SQUARE + const.WALL_W), board_rect.top + i * (const.SQUARE + const.WALL_W), const.SQUARE, const.SQUARE)
-            pygame.draw.rect(screen, const.SQUARE_COLOR, square)
+            if [i, j] in start_positions[0]:
+                pygame.draw.rect(screen, const.START_RED_COLOR, square)
+            elif [i, j] in start_positions[1]:
+                pygame.draw.rect(screen, const.START_YELLOW_COLOR, square)
+            else:
+                pygame.draw.rect(screen, const.SQUARE_COLOR, square)
 
     #crtamo igrace na odgovarajucim pozicijama
     p_1_fig_1 = player_1[0]
     c_x = board_rect.left + int(p_1_fig_1[1]) * (const.SQUARE + const.WALL_W) + const.SQUARE // 2
     c_y = board_rect.top + int(p_1_fig_1[0]) * (const.SQUARE + const.WALL_W) + const.SQUARE // 2
-    p1_f1_rect = pygame.draw.circle(screen, "RED", (c_x, c_y), const.SQUARE // 3)
+    p1_f1_rect = pygame.draw.circle(screen, const.RED_PLAYER, (c_x, c_y), const.SQUARE // 3)
 
     p_1_fig_2 = player_1[1]
     c_x = board_rect.left + int(p_1_fig_2[1]) * (const.SQUARE + const.WALL_W) + const.SQUARE // 2
     c_y = board_rect.top + int(p_1_fig_2[0]) * (const.SQUARE + const.WALL_W) + const.SQUARE // 2
-    p1_f2_rect = pygame.draw.circle(screen, "RED", (c_x, c_y), const.SQUARE // 3)
+    p1_f2_rect = pygame.draw.circle(screen, const.RED_PLAYER, (c_x, c_y), const.SQUARE // 3)
 
     p_2_fig_1 = player_2[0]
     c_x = board_rect.left + int(p_2_fig_1[1]) * (const.SQUARE + const.WALL_W) + const.SQUARE // 2
     c_y = board_rect.top + int(p_2_fig_1[0]) * (const.SQUARE + const.WALL_W) + const.SQUARE // 2
-    p2_f1_rect = pygame.draw.circle(screen, "YELLOW", (c_x, c_y), const.SQUARE // 3)
+    p2_f1_rect = pygame.draw.circle(screen, const.YELLOW_PLAYER, (c_x, c_y), const.SQUARE // 3)
 
     p_2_fig_2 = player_2[1]
     c_x = board_rect.left + int(p_2_fig_2[1]) * (const.SQUARE + const.WALL_W) + const.SQUARE // 2
     c_y = board_rect.top + int(p_2_fig_2[0]) * (const.SQUARE + const.WALL_W) + const.SQUARE // 2
-    p2_f2_rect = pygame.draw.circle(screen, "YELLOW", (c_x, c_y), const.SQUARE // 3)
+    p2_f2_rect = pygame.draw.circle(screen, const.YELLOW_PLAYER, (c_x, c_y), const.SQUARE // 3)
 
     DrawWalls(board_rect, walls)
 
@@ -156,6 +166,7 @@ def DrawGameBoard(size: tuple[int, int], player_1: tuple[tuple[int, int]], playe
 
 
 #game loop
+game_end = False
 red_to_move = True
 #move_state je lista koja pamti stanje poteza (prvi element pamti da li je izabran igrac kog zelimo da pomerimo, drugi element pamti da li je izabrano odredisno polje,
 #treci element pokazuje da li je izabran zid). Kada su sva tri elementa True, potez je odigran i prelazimo na drugog igraca
@@ -268,6 +279,18 @@ def HandleClickEvent(click_pos : tuple[int, int], board_info):
         pygame.event.post(pygame.event.Event(MOVE_FINISHED))
 
 
+#funkcija za prikaz pobednika na kraju igre
+def PrintWinner(winner_red: bool):
+    screen.fill("BLACK")
+    winner_font = pygame.font.Font(None, const.WIN_FONT_SIZE)
+    winner_color = "crveni" if winner_red else "zuti"
+    winner_text = "Cestitamo, pobedio je " + winner_color + " igrac!"
+    winner_text_surface = winner_font.render(winner_text, True, const.WIN_TEXT_COLOR)
+    winner_text_rect = winner_text_surface.get_rect(center = (const.WIN_WIDTH // 2, const.WIN_HEIGHT // 2))
+    screen.blit(winner_text_surface, winner_text_rect)
+
+
+
 
 while True:
 
@@ -285,25 +308,36 @@ while True:
             wall_move_pos = (move[2][0], move[2][1]) if move[2] else ()
             wall_type = move[2][2] if move[2] else ''
             player_type = 'x' if red_to_move else 'o'
-            game.nextMove(move[0], wall_move_pos, wall_type, player_type, move[1])
-
-            #promena stanja
-            if (game.board.player1.greenWallNumber == game.board.player2.blueWallNumber == 0) and (game.board.player2.greenWallNumber == game.board.player2.blueWallNumber == 0):
-                #ako nemamo vise zidova
-                new_state = [False, False, True]
+            move_done = game.nextMove(move[0], wall_move_pos, wall_type, player_type, move[1])
+            if game.isEnd():
+                game_end = True
             else:
-                new_state = [False, False, False]
-            
-            red_to_move = not red_to_move
-            move = [None, None, None]
-            move_state = new_state
-            #ovde treba da ubacimo proveru da li je stiglo do kraja
-            print(red_to_move)
+                if not move_done:
+                    #ukoliko je potez nevalidan, moramo da resetujemo potez
+                    move = [None, None, None]
+                    old_state = False if wall_type != '' else True
+                    move_state = [False, False, old_state]
+
+                else:
+                    #promena stanja
+                    if (game.board.player1.greenWallNumber == game.board.player2.blueWallNumber == 0) and (game.board.player2.greenWallNumber == game.board.player2.blueWallNumber == 0):
+                        #ako nemamo vise zidova
+                        new_state = [False, False, True]
+                    else:
+                        new_state = [False, False, False]
+                    
+                    red_to_move = not red_to_move
+                    move = [None, None, None]
+                    move_state = new_state
             
 
-    #draw
-    DrawHeader((game.board.player1.greenWallNumber, game.board.player1.blueWallNumber), (game.board.player1.greenWallNumber, game.board.player1.blueWallNumber), game.isPlayerOneNext)
-    board_info = DrawGameBoard((game.board.m, game.board.n), game.board.player1.positions, game.board.player2.positions, game.board.walls)
+    if not game_end:
+        #draw
+        DrawHeader((game.board.player1.greenWallNumber, game.board.player1.blueWallNumber), (game.board.player2.greenWallNumber, game.board.player2.blueWallNumber), red_to_move)
+        board_info = DrawGameBoard((game.board.m, game.board.n), game.board.player1.positions, game.board.player2.positions, game.board.walls, (game.board.startPositionsX, game.board.startPositionsO))
+    else:
+        PrintWinner(red_to_move)
+
 
     #update
     pygame.display.update()
